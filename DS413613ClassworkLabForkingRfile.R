8de20d42dc636d1872bd54b8bfedb26529da8d01
thezaro <98924064+thezaro@users.noreply.github.com>
2022-03-16 17:59:02 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is Dennis P.
+# My name is MZ P.
 #The data source is the mpg data table
-# This is my coding that generates Boxplots for transmission types
-ggplot(data = mpg) +
+# This is my coding that generates Boxplots for transmission types 
+ggplot(data = Flights13) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
